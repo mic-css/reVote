a3142de7fca3bddd5dea8a0550afe3a3479466b1
Mic Cassano <mic.cassano@gmail.com>
2016-11-08 17:54:11 +0000

    Add socket event emitters on connection and action

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import makeStore from './src/store';
 import startServer from './src/server';
+import entries from './entries';
 
 export const store = makeStore();
-startServer();
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: entries
+});
+
+store.dispatch({type: 'NEXT'});
